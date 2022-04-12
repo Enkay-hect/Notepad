f11147faef04a93df0e63c44b8cb44fc1766e541
Enkay-hect <fridaynkeneabasi@gmail.com>
2022-04-12 18:06:10 +0100

    Created database connection

---
+++ phpinc/dbcon.php
@@ -0,0 +1,17 @@
+<?php
+ $servername = "localhost";
+ $username = "Enkay";
+ $password = "07080987528";
+ $database = "notepad";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+?> 
