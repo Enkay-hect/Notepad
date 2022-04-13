184a904ac2507bac41292b338bfb0c5ce99e8b7a
MrMiles94 <aniefiokasuquo33@gmail.com>
2022-04-13 21:28:50 +0100

    The TO DOs are completed. please check and comment

--- phpinc/user.php
+++ phpinc/user.php
@@ -20,6 +20,7 @@ include_once "helpers/create_tables.php";
         $this->user_name = $user_name;
         $this->email = $email;
         $this->password = $password;
+        $this->sql   = "SELECT * FROM `users` WHERE user_id='$user_name' AND password='$password'";
     }
 
     private function create_table(){
@@ -42,36 +43,36 @@ include_once "helpers/create_tables.php";
     public function email(){
         return $this->email;
     }
+    public function sql_connect(){
+        $dbcon = new DatabaseConnection();
+
+        $result = mysqli_query($dbcon->connect(), $this->sql)  or die('Access denied! <br>');
+        $rows = mysqli_num_rows($result);
+        if($rows == 1){
+            $_SESSION['username'] = $this->username;
+            $_SESSION['password'] = $this->password;
+            // go to dashboard when the login is successful
+            header("location: ../backend/index.html");
+        }
+        else{
+            // stay in the login page when the login is not successful
+           // header("location: login.php");
+        }
+    }
+    public function login_message(){
+        $login_message = " Don't have an account?";
+        if (!$this->sql_connect()){
+            $login_message = "Are you sure you have an account with us? Maybe ";  
+        }
+        return $login_message;
+    }
 
 }
 
 
 
   /// TO DO :: Move this( The connection and sql query) into a class function for $login_message
+  
   /// TO DO :: REDIRECT TO INDEX PAGE AFTER SUCCESSFUL LOGIN..
 
-    $login_message = " Don't have an account?";
-    // Check if the form is submitted
-    if(isset($_REQUEST['username']) && isset($_REQUEST['password'])){
-        // Remove back slashes from the input
-        $username = stripslashes($_REQUEST['username']) ;
-        $password = stripslashes($_REQUEST['password']) ;
-
-
-        $username = mysqli_real_escape_string($conn, $_REQUEST['username']);
-        $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
-
-        $sql   = "SELECT * FROM `users` WHERE user_id='$username' AND password='$password'";
-        $result = mysqli_query($conn, $sql)  or die('Access denied! <br>');
-        $rows = mysqli_num_rows($result);
-        if($rows == 1){
-            $_SESSION['username'] = $username;
-            $_SESSION['password'] = $password;
-            header("location: dashboard.php");
-        }
-        else{
-            $login_message = "Are you sure you have an account with us? Maybe ";
-            header("location: login.php");
-        }
-    }
-    ?>
+?>
